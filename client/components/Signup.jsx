6784c93bae1248aa3d6b939aa4643b97ec75d304
refactoredjello <gil.mandler@gmail.com>
2017-11-09 09:46:16 -0500

    Basic structure for login and signup

---
+++ client/components/Signup.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import axios from 'axios'
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username = '',
+      password = ''
+    }
+  }
+  handleUsername (event) {
+    this.setState({username: event.target.value})
+  }
+  handlePassowrd(even) {
+    this.setState({password: event.target.value})
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    axios.post(`/user?username=${this.state.username}&password=${this.state.password}`)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    }) 
+  }
+  render() {
+    (
+      <div className="signup">
+        <h2>Login</h2>
+        <form onSubmit={this.handleSubmit}>
+        <label>Username:
+          <input type="text" onChange={this.handleUsername} />
+        </label>
+        <label>Password:
+          <input type="text" onChange={this.handlePassowrd} />
+          <input type="submit" value="Submit" />
+        </label>
+        </form>
+      </div>
+    )
+  }
+}
