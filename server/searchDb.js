405a23c21ba35cffc9f6718aa518205b44298b9c
refactoredjello <gil.mandler@gmail.com>
2017-11-10 15:29:37 -0500

    Refactor for code simplicity

--- server/searchDb.js
+++ server/searchDb.js
@@ -1,13 +1,10 @@
 const Communties = require('./db.js').Communities
 
-// Search the communties table for community centers matching the query
+// Search the communties table for community centers matching the borough query
 module.exports = (borough) => {
   let searchParam = { where: { borough: borough } };
   if (borough === 'All') searchParam = {};
   return Communties.findAll(searchParam)
-    .then(results => {
-     return results
-    })
 }
 
 
